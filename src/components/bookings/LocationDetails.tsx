1080e55523e1e5dbc0f7a50542d7c6b81dad467d
Jay330-commits <mathahaj@gmail.com>
2026-02-22 21:37:12 +0100

    upadate25

--- src/components/bookings/LocationDetails.tsx
+++ src/components/bookings/LocationDetails.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect, useMemo, useCallback, useRef } from 'react';
 import { calculateBookingDays, calculateBookingTotal } from '@/utils/bookingPrice';
 import Image from 'next/image';
